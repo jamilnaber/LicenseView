9281bda92d8efe22c716d1db714304adf770218f
jamilnaber <jamilnaber.school@gmail.com>
2021-03-20 05:29:38 -0500

    Added cameraX, next will be capture and store image

--- app/src/main/java/com/example/licenseview/CaptureFragment.java
+++ app/src/main/java/com/example/licenseview/CaptureFragment.java
@@ -1,28 +1,64 @@
 package com.example.licenseview;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link CaptureFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+
 public class CaptureFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private Preview preview;
+    private ImageCapture imageCapture;
+    private ImageAnalysis imageAnalysis;
+    private Camera camera;
+    private Context safeContext;
+    private File outputFile;
+    private ExecutorService cameraExecutor;
+    PreviewView mPreviewView;
 
     public CaptureFragment() {
         // Required empty public constructor
@@ -45,6 +81,20 @@ public class CaptureFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        safeContext = context;
+    }
+    
+    private int getStatusBarHeight(){
+        int resourceId = safeContext.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if(resourceId > 0){
+            return safeContext.getResources().getDimensionPixelSize(resourceId);
+        }
+        return 0;
+    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,5 +110,62 @@ public class CaptureFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_capture, container, false);
+
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState ){
+        super.onViewCreated(view,savedInstanceState);
+        mPreviewView = (PreviewView) view.findViewById(R.id.viewFinder);
+        startCamera();
+    }
+
+    private void startCamera() {
+        final ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(safeContext);
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                    bindPreview(cameraProvider);
+
+                } catch (InterruptedException | ExecutionException e) {
+                    // No errors need to be handled for this Future.
+                    // This should never be reached.
+                }
+            }
+        }, ContextCompat.getMainExecutor(safeContext));
+    }
+
+
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .build();
+
+        ImageCapture.Builder builder = new ImageCapture.Builder();
+
+//        //Vendor-Extensions (The CameraX extensions dependency in build.gradle)
+//        HdrImageCaptureExtender hdrImageCaptureExtender = HdrImageCaptureExtender.create(builder);
+//
+//        // Query if extension is available (optional).
+//        if (hdrImageCaptureExtender.isExtensionAvailable(cameraSelector)) {
+//            // Enable the extension if available.
+//            hdrImageCaptureExtender.enableExtension(cameraSelector);
+//        }
+
+        preview.setSurfaceProvider(mPreviewView.createSurfaceProvider());
+
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview, imageAnalysis);
+
+    }
+
 }
--- app/src/main/java/com/example/licenseview/LicenseActivity.java
+++ app/src/main/java/com/example/licenseview/LicenseActivity.java
@@ -1,10 +1,15 @@
 package com.example.licenseview;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -19,7 +24,11 @@ public class LicenseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_license);
         bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
-        openFragment(CaptureFragment.newInstance("", ""));
+        if (checkPermission()){
+            openFragment(CaptureFragment.newInstance("", ""));
+        }
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 
     BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
@@ -48,5 +57,29 @@ public class LicenseActivity extends AppCompatActivity {
         transaction.addToBackStack(null);
         transaction.commit();
     }
+
+    private boolean checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA )
+                == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+        else{
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0) ;
+        }
+        return false;
+    }
+
+    public void onRequestPermissionsResult(int code, String[] permissions, int[] results) {
+        if (results.length > 0) {
+            if (results[0] == PackageManager.PERMISSION_GRANTED) {
+
+            }
+            else {
+
+            }
+        }
+    }
+
+
 
 }
